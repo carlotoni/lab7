0e7cc94a297cf4b76f22b8faaaab31df79342f00
Carlo Mendoza <carlomendoza89@gmail.com>
2019-03-28 15:18:44 -0700

    push for tests

--- app/Controllers/Client.php
+++ app/Controllers/Client.php
@@ -6,43 +6,29 @@ use CodeIgniter\HTTP\CURLRequest;
 class Client extends Controller
 {
     public function index() {
-        # some data, from a form perhaps
-//        $order = $_POST['TEST'];
-//        $quantity = $_POST[9];
-//        $price = $_POST[10];
+        $client = \Config\Services::curlrequest();
 
-# data needs to be POSTed to the service
-        $data = array("order" => "TEST2", "quantity" => 9, "price" => 10);
-        $data_string = json_encode($data);
+//        $xml = new \SimpleXMLElement('<order/>');
+//        $xml -> order = 'test2';
+//        $xml -> quantity = 2;
+//        $xml -> price = 12;
 
-        $ch = curl_init('http://localhost:8080/server/work');
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-                'Content-Type: application/json',
-                'Content-Length: ' . strlen($data_string))
-        );
-        curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+//        $json = json_encode(array("test3", 3, 13));
 
-//execute post
-        $result = curl_exec($ch);
+//        echo json_encode($xml);
+//        echo $json;
 
-//close connection
-        curl_close($ch);
+        $response = $client->request('POST', 'http://localhost:8080/server/work', [
+            'form_params' => [
+                'order' => 'test',
+                'quantity' => 1,
+                'price' => 11]
+//            $xml,
+//            $json
+        ]);
 
-        return $data_string;
+        return $response->getBody();
     }
-
-//    public function index() {
-//        $client = \Config\Services::curlrequest();
-//        $response = $client->request('POST', 'localhost:8080/server/work', [
-//            'user_input' => ['order' => 'test', 'quantity' => '999', 'price' => '10000']
-//        ]);
-//
-//        return $response->getBody();
-//    }
 }
 
 
