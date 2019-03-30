c35ca7b655e258d00aeed9a671a13023e6c5c80e
Carlo Mendoza <carlomendoza89@gmail.com>
2019-03-30 16:20:17 -0700

    changing back to previous form

--- app/Controllers/Client.php
+++ app/Controllers/Client.php
@@ -8,11 +8,11 @@ class Client extends Controller
     public function index() {
         $client = \Config\Services::curlrequest();
 
-        $form = [
-            'order' => 'test',
-            'quantity' => 1,
-            'price' => 11
-        ];
+//        $form = [
+//            'order' => 'test',
+//            'quantity' => 1,
+//            'price' => 11
+//        ];
 
 //        $xml = new \SimpleXMLElement('<order/>');
 //        $xml -> order = 'test2';
@@ -22,11 +22,11 @@ class Client extends Controller
 //        $json = json_encode(array("test3", 3, 13));
 
         $response = $client->request('POST', 'http://localhost:8080/server/work', [
-//            'form_params' => [
-//                'order' => 'test',
-//                'quantity' => 1,
-//                'price' => 11],
-            $form
+            'form_params' => [
+                'order' => 'test',
+                'quantity' => 1,
+                'price' => 11]
+//            $form
 //            $xml,
 //            $json
         ]);
