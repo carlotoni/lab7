3f2a162391764139d09cb28cda631a2e4f664283
Carlo Mendoza <carlomendoza89@gmail.com>
2019-03-30 19:12:40 -0700

    back to 1 message

--- app/Controllers/Client.php
+++ app/Controllers/Client.php
@@ -24,24 +24,13 @@ class Client extends Controller
 
         $response = $client->request('POST', 'http://localhost:8080/server/work', [
             'form_params' => [
-                'order' => 'test1',
-                'quantity' => 1,
-                'price' => 11]
+                    'order' => 'test1',
+                    'quantity' => 1,
+                    'price' => 11]
         ]);
 
 //        $response = $client ->setBody(serialize($message))
 //                            ->request('POST', 'http://localhost:8080/server/work');
-
-//        $client->request('POST', 'http://localhost:8080/server/work', [
-//            'form_params' =>
-//                [
-//                    [
-//                    'order' => 'test',
-//                    'quantity' => 1,
-//                    'price' => 11
-//                    ]
-//                ]
-//        ]);
 
         return array($response->getBody());
     }
