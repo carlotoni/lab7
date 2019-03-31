166c1010c725c709ed74bfd68bafd781ab5860f8
Carlo Mendoza <carlomendoza89@gmail.com>
2019-03-30 23:22:26 -0700

    echo post

--- app/Controllers/Server.php
+++ app/Controllers/Server.php
@@ -13,40 +13,13 @@ class Server extends Controller
         parent::initController($request, $response, $logger);
     }
 
-    protected $counter = 1;
+    private $counter = 1;
 
     public function work() {
 
-//        $form = [
-//            'order' => 'test',
-//            'quantity' => 1,
-//            'price' => 11
-//        ];
-//
-//        $xml = new \SimpleXMLElement('<order/>');
-//        $xml -> order = 'test2';
-//        $xml -> quantity = 2;
-//        $xml -> price = 12;
-//
-//        $json = array(
-//            'order'=>'test3',
-//            'quantity'=>3,
-//            'price'=>13
-//        );
-//
-//        $form = json_encode($form);
-//        $xml = json_encode($xml);
-//        $json = json_encode($json);
-
-//        $data = array($_REQUEST);
-
-        echo $this->counter++;
-//
-//        foreach ($_POST as $order)
-//        {
-//            echo $this->counter;
-//            echo $order;
-//        }
+        echo $this->counter;
+        echo $_POST;
+        $this->counter++;
 
         return json_encode($_POST);
     }
