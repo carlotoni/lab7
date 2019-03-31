a9b2d7f9f3cebd54d5ce9457bbcfc10d0459183f
Carlo Mendoza <carlomendoza89@gmail.com>
2019-03-30 17:03:22 -0700

    client string cast

--- app/Controllers/Client.php
+++ app/Controllers/Client.php
@@ -8,36 +8,36 @@ class Client extends Controller
     public function index() {
         $client = \Config\Services::curlrequest();
 
-//        $form = [
-//            'order' => 'test',
-//            'quantity' => 1,
-//            'price' => 11
-//        ];
-//
-//        $xml = new \SimpleXMLElement('<order/>');
-//        $xml -> order = 'test2';
-//        $xml -> quantity = 2;
-//        $xml -> price = 12;
-//
-//        $json = json_encode(array("test3", 3, 13));
-//
-//        $body = array($form, $xml, $json);
+        $form = [
+            'order' => 'test',
+            'quantity' => 1,
+            'price' => 11
+        ];
 
-        $response = $client->request('POST', 'http://localhost:8080/server/work', [
-            'form_params' =>
-                [
-                    'order' => 'test',
-                    'quantity' => 1,
-                    'price' => 11
-                ],
-                [
-                    'order' => 'test2',
-                    'quantity' => 2,
-                    'price' => 22
-                ]
-        ]);
+        $xml = new \SimpleXMLElement('<order/>');
+        $xml -> order = 'test2';
+        $xml -> quantity = 2;
+        $xml -> price = 12;
 
-//        $response = $client->request('POST', 'http://localhost:8080/server/work', $body);
+        $json = json_encode(array("test3", 3, 13));
+
+        $body = array($form, $xml, $json);
+
+//        $response = $client->request('POST', 'http://localhost:8080/server/work', [
+//            'form_params' =>
+//                [
+//                    'order' => 'test',
+//                    'quantity' => 1,
+//                    'price' => 11
+//                ],
+//                [
+//                    'order' => 'test2',
+//                    'quantity' => 2,
+//                    'price' => 22
+//                ]
+//        ]);
+
+        $response = $client->request('POST', 'http://localhost:8080/server/work', (string) $body);
 
         return $response->getBody();
     }
